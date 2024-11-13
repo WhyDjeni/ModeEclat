fe807be32438443e2116f7a8a63fd55cc39421d4
Filipe Ferreira <felipesilvaferreira372@gmail.com>
2024-11-13 18:37:38 -0300

    Normalize line endings

---
+++ modeEclat/pages/components/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ModeEclat from './index.js';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <ModeEclat />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
---
+++ modeEclat/pages/components/men.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ModeEclat from './men.js';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <ModeEclat />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
