d0cb8cdacc124747bdb9d9dea19781909b5569e5
Filipe Ferreira <felipesilvaferreira372@gmail.com>
2024-12-02 15:45:38 -0300

    Normalize line endings

--- MODE_ECLAT_v2/ModeEclat-main/js/javascript.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("shop-now").addEventListener("click", function() {
-    alert("Redirecionando para a seção de produtos!");
-    window.location.href = "#produtos";
-});
-
-// Função que alterna a classe 'favorited' para alterar a cor do coração
-function toggleFavorite(element) {
-    element.classList.toggle('favorited');
-}
