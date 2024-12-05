932222fbf4e234085278faefb0bf3ee78eb91b47
Filipe Ferreira <felipesilvaferreira372@gmail.com>
2024-12-05 02:00:43 -0300

    Normalize line endings

--- Mode_Eclat_v1/pages/scripts.js
+++ Mode_Eclat_v1/pages/scripts.js
@@ -22,14 +22,14 @@ document.getElementById("camisa4").addEventListener("click", function() {
 });
 
 document.getElementById("blusa1").addEventListener("click", function() {
-  window.location.href = "./roupas_mulher/blusa1.html"; 
+  window.location.assign = "./roupas_mulher/blusa1.html"; 
 })
 document.getElementById("blusa2").addEventListener("click", function() {
-  window.location.href = "./roupas_mulher/blusa2.html"; 
+  window.location.assign = "./roupas_mulher/blusa2.html"; 
 })
 document.getElementById("blusa3").addEventListener("click", function() {
-  window.location.href = "./roupas_mulher/blusa3.html"; 
+  window.location.assign = "./roupas_mulher/blusa3.html"; 
 })
 document.getElementById("blusa4").addEventListener("click", function() {
-  window.location.href = "./roupas_mulher/blusa4.html"; 
+  window.location.assign = "./roupas_mulher/blusa4.html"; 
 })
