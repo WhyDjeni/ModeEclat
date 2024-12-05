e49d52042ce4138cae2287e0b5a7744317a09dfb
Filipe Ferreira <felipesilvaferreira372@gmail.com>
2024-12-05 03:12:35 -0300

    Normalize line endings

--- Mode_Eclat_v1/pages/script2.js
+++ Mode_Eclat_v1/pages/script2.js
@@ -10,8 +10,30 @@ document.addEventListener('DOMContentLoaded', function() {
   addRedirectListener("blusa3", "./roupas_mulher/blusa3.html");
   addRedirectListener("blusa4", "./roupas_mulher/blusa4.html");
 
-  addRedirectListener("camisa1", "./roupas_homem/camisa1.html");
-  addRedirectListener("camisa2", "./roupas_homem/camisa2.html");
-  addRedirectListener("camisa3", "./roupas_homem/camisa3.html");
-  addRedirectListener("camisa4", "./roupas_homem/camisa4.html");
+});
+document.addEventListener('DOMContentLoaded', function() {
+  document.getElementById("camisa1").addEventListener("click", function() {
+      window.location.href = "./roupas_homem/camisa1.html"; 
+  });
+  document.getElementById("camisa2").addEventListener("click", function() {
+      window.location.href = "./roupas_homem/camisa2.html"; 
+  });
+  document.getElementById("camisa3").addEventListener("click", function() {
+      window.location.href = "./roupas_homem/camisa3.html"; 
+  });
+  document.getElementById("camisa4").addEventListener("click", function() {
+      window.location.href = "./roupas_homem/camisa4.html"; 
+  });
+  document.getElementById("camisa5").addEventListener("click", function() {
+    window.location.href = "./roupas_homem/camisa5.html"; 
+  });
+  document.getElementById("camisa6").addEventListener("click", function() {
+    window.location.href = "./roupas_homem/camisa6.html"; 
+  });
+  document.getElementById("camisa7").addEventListener("click", function() {
+    window.location.href = "./roupas_homem/camisa7.html"; 
+  });
+  document.getElementById("camisa8").addEventListener("click", function() {
+    window.location.href = "./roupas_homem/camisa8.html"; 
+  });
 });
